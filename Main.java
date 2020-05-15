c5328d20e6968478e035db76e5ae4a848cb8f32e
IgorZimenkov <44290255+IgorZimenkov@users.noreply.github.com>
2020-05-15 13:51:42 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,65 +2,65 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static double doMath(double a, double b, double c, double d) {
-            return a * (b + (c / d));
-        }
-
-
-    public static boolean ifTrue(float a, float b) {
-        if (a + b < 20 && a + b > 10) {
-            return (true);
-        } else {
-            return (false);
-        }
-    }
-
-    public static void isPositiveOrNegative(int a) {
-        if (a < 0) {
-            System.out.println("Negative");
-        } else {
-            System.out.println("Positive");
-        }
-    }
-
-    public static boolean isNegative(int a) {
-        if (a < 0) {
-            return (true);
-        } else {
-            return (false);
-        }
-    }
-
-    public static void printHello() {
-        Scanner one = new Scanner(System.in);
-        System.out.println("Input your name:");
-        String name = one.nextLine();
-        System.out.println("Hello, " + name);
-    }
-
-    public static void isLeap(int a) {
-        if (a % 4 == 0 || a % 400 == 0 && a % 100 != 0) {
-            System.out.println("The " + a + " is leap");
-        } else {
-            System.out.println("The " + a + " is not leap");
-        }
-    }
-
     public static void main(String[] args) {
-        int Int = 1;
-        char Char = 'C';
-        float Float = 1.0f;
-        double Double = 1.000000000000000001;
-        boolean Boolean = true;
-        byte Byte = 1;
-        long Long = 123456776L;
 
+        //1. Написать программу, которая загадывает случайное число от 0 до 9, и пользователю дается 3 попытки угадать это число.
+        // При каждой попытке компьютер должен сообщить больше ли указанное пользователем число чем загаданное, или меньше.
+        // После победы или проигрыша выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
+        boolean tryAgain = false;
+        do {
+            int number = (int) Math.round(Math.random() * 9);
+            int counter = 0;
+            do {
+                Scanner request = new Scanner(System.in);
+                System.out.println("Ввеидте число от 0 до 9");
+                String input = request.next();
+                int inputed = Integer.parseInt(input);
+                if (inputed == number) {
+                    System.out.println("Вы угадали!");
+                    break;
+                } else {
+                    if (inputed < number) {
+                        System.out.println("Загаданное число больше введённого.");
+                    } else if (inputed > number) {
+                        System.out.println("Загаданное число меньше введённого.");
+                    } else if (inputed < 0 || inputed > 9) {
+                        System.out.println("Введено не верное значение.");
+                        counter = counter - 1;
+                    } else {
+                        System.out.println("Введено не число.");
+                        counter = counter - 1;
+                    }
+                }
+                counter++;
+            } while (counter < 3 );
+            System.out.println("Загаданное число: " + number);
+            System.out.println();
+            System.out.println("Хотите сыграть ещё раз? 1 - да, другое - нет:");
+            Scanner scanner = new Scanner(System.in);
+            String inputed = scanner.next();
+            if (inputed == "1") {
+                tryAgain = true;
+            } else {
+                tryAgain = false;
+            }
+        } while (tryAgain == true);
 
-        System.out.println(doMath(10, 20, 30, 11));
-        System.out.println(ifTrue(14, 11));
-        isPositiveOrNegative(-10);
-        System.out.println(isNegative(10));
-        printHello();
-        isLeap(2015);
     }
 }
+
+
+    //2 * Создать массив из слов String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
+    // "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
+    // "pear", "pepper", "pineapple", "pumpkin", "potato"};
+    //При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
+    //сравнивает его с загаданным словом и сообщает правильно ли ответил пользователь. Если слово не угадано,
+    // компьютер показывает буквы которые стоят на своих местах.
+    //apple – загаданное
+    //apricot - ответ игрока
+    //ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
+    //Для сравнения двух слов посимвольно, можно пользоваться:
+    //String str = "apple";
+    //str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции
+    //Играем до тех пор, пока игрок не отгадает слово
+    //Используем только маленькие буквы
